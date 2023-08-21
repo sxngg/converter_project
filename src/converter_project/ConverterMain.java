d35fa823ca30557c6cc1b6a30f6235a7198c4e36
sxngg <santiagogarciagarzon.8@gmail.com>
2023-08-21 10:24:42 -0500

    First commit, init project

---
+++ src/converter_project/ConverterMain.java
@@ -0,0 +1,7 @@
+package converter_project;
+
+public class ConverterMain {
+	public static void main(String[] args) {
+		System.out.println("Init");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module converter_project {
+}
